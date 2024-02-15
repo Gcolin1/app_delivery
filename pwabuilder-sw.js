bf9169fb5f5723e529b1217debc3de60518cb20e
Gcolin1 <guilhermecolin09@gmail.com>
2024-02-14 21:25:06 -0300

    pwa

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,10 +1,7 @@
-// This is the "Offline page" service worker
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
+const CACHE_NAME = "pwabuilder-page";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
@@ -15,7 +12,7 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
+    caches.open(CACHE_NAME)
       .then((cache) => cache.add(offlineFallbackPage))
   );
 });
@@ -38,7 +35,7 @@ self.addEventListener('fetch', (event) => {
         return networkResp;
       } catch (error) {
 
-        const cache = await caches.open(CACHE);
+        const cache = await caches.open(CACHE_NAME);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
       }
